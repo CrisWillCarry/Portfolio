7bff0c6ae207207a44a1b0d9540ee799a255a0bc
Cristian <cf93082@gmail.com>
2024-02-15 18:42:21 -0500

    fixing skills responsiveness

--- frontend/src/SkillsDisplay.tsx
+++ frontend/src/SkillsDisplay.tsx
@@ -15,7 +15,7 @@ const SkillsDisplay: React.FC = () => {
                 <span className="text-blue-500">My</span>
                 <span className="text-green-500"> Skills</span>
             </h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  gap-4 place-items-center mt-10">
+            <div className="grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4  gap-4 place-items-center mt-10">
                 {skills.map((skill) => (
                     <SkillBox
                         key={skill.id}
