25984f76c7c1769b81c5f41dda850cb49c1530d9
Cristian <cf93082@gmail.com>
2024-02-15 18:53:26 -0500

    fixed reviews responsiveness

--- frontend/src/Reviews.tsx
+++ frontend/src/Reviews.tsx
@@ -64,55 +64,56 @@ const Reviews: React.FC = () => {
 
     return (
         <div id="reviews" className="mt-10 flex justify-center items-center">
-            <div className="w-2/3 xl:ml-32 md:ml-28 sm:ml-9 text-white border rounded-xl bg-blue-500 shadow-lg">
-            <h1 className="font-bold text-7xl mb-8">
-    
-                Portfolio{" "}
-                {rainbowColors.map((color, index) => (
-                    <span key={index} style={{ color }}>
-                        {index === 0 ? "R" : index === 1 ? "e" : index === 2 ? "v" : index === 3 ? "i" : index === 4 ? "e" : index === 5 ? "w" : "s"}
-                    </span>
-                ))}
-            </h1>
+            <div className="w-full sm:w-5/6 md:w-4/6 lg:w-3/6 xl:w-2/3 text-white border rounded-xl bg-blue-500 shadow-lg p-4">
+                <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl mb-8">
+                    Portfolio{" "}
+                    {rainbowColors.map((color, index) => (
+                        <span key={index} style={{ color }}>
+                            {index === 0 ? "R" : index === 1 ? "e" : index === 2 ? "v" : index === 3 ? "i" : index === 4 ? "e" : index === 5 ? "w" : "s"}
+                        </span>
+                    ))}
+                </h1>
 
-                <div className="flex flex-row items-center justify-center">
-                    {currentIndex > 0 && (
-                        <button className="mr-4" onClick={goToPreviousReview}>
-                            <span role="img" className="text-3xl" aria-label="Left Arrow">⬅️</span>
-                        </button>
-                    )}
-                    {reviews.length > 0 && (
-                        <div className={`transition-opacity ${isTransitioning ? "opacity-0" : "opacity-100"}`}>
-                            <ReviewBlock
-                                key={currentIndex}
-                                comment={(reviews[currentIndex] as { comment: string }).comment}
-                                stars={(reviews[currentIndex] as { stars: number }).stars}
-                                name={(reviews[currentIndex] as { username: string }).username}
-                                picture={(reviews[currentIndex] as { picture: string }).picture}
-                                date={(reviews[currentIndex] as { date: string }).date}
-                            />
+                <div className="flex flex-col items-center">
+                    <div className="mb-4">
+                        {currentIndex > 0 && (
+                            <button className="mr-4" onClick={goToPreviousReview}>
+                                <span role="img" className="text-2xl sm:text-3xl" aria-label="Left Arrow">⬅️</span>
+                            </button>
+                        )}
+                        {reviews.length > 0 && (
+                            <div className={`transition-opacity ${isTransitioning ? "opacity-0" : "opacity-100"}`}>
+                                <ReviewBlock
+                                    key={currentIndex}
+                                    comment={(reviews[currentIndex] as { comment: string }).comment}
+                                    stars={(reviews[currentIndex] as { stars: number }).stars}
+                                    name={(reviews[currentIndex] as { username: string }).username}
+                                    picture={(reviews[currentIndex] as { picture: string }).picture}
+                                    date={(reviews[currentIndex] as { date: string }).date}
+                                />
+                            </div>
+                        )}
+                        {currentIndex < reviews.length - 1 && (
+                            <button className="ml-4" onClick={goToNextReview}>
+                                <span role="img" className="text-2xl sm:text-3xl" aria-label="Right Arrow">➡️</span>
+                            </button>
+                        )}
+                    </div>
+                    {!isLoggedIn ? (
+                        <div className="flex flex-col justify-center items-center">
+                            <LoginButton onLogin={handleLogin} />
+                            <div className="text-sm">*Needed to leave a review*</div>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col justify-center items-center">
+                            <button className="bg-green-500 text-white px-4 py-2 rounded-md" onClick={handleLeaveReviewClick}>
+                                Leave a Review
+                            </button>
                         </div>
                     )}
-                    {currentIndex < reviews.length - 1 && (
-                        <button className="ml-4" onClick={goToNextReview}>
-                            <span role="img" className="text-3xl" aria-label="Right Arrow">➡️</span>
-                        </button>
-                    )}
                 </div>
-                {!isLoggedIn ? (
-                    <div className="flex flex-col justify-center mt-4 mb-4 items-center">
-                        <LoginButton onLogin={handleLogin} />
-                        <div>*Needed to leave a review*</div>
-                    </div>
-                ) : (
-                    <div className="flex flex-col justify-center mt-4 mb-4 items-center">
-                        <button className="bg-green-500 text-white px-4 py-2 rounded-md" onClick={handleLeaveReviewClick}>
-                            Leave a Review
-                        </button>
-                    </div>
-                )}
             </div>
-            {/* Conditionally render the review form modal */}
+            
             {showReviewForm && (
                 <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 flex justify-center items-center">
                     <ReviewForm onClose={handleLeaveReviewClick}/>
